7c898bde74c596195c9924af3f81e1baef3758d8
Daniel <d.akipskiy@gmail.com>
2025-06-15 20:19:43 +0800

    feat(config): добавить подгрузку .env

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as hotels_router
+from src.config import settings
 
 app = FastAPI()
 
