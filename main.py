aeb59d006ca72e1c084cfd87b1e27989dd2262a7
Daniel <d.akipskiy@gmail.com>
2025-06-15 10:56:59 +0800

    init

---
+++ main.py
@@ -0,0 +1,31 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.openapi.docs import (
+    get_redoc_html,
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
+
+app = FastAPI(docs_url=None, redoc_url=None)
+
+
+# Исправление долгой загрузки /docs. Взято из документации.
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+    )
+
+
+@app.get("/")
+async def root():
+    return {"message": " sddHello World!!!!!!"
+                       "!!!!!!!"}
+
+
+if __name__ == "__main__":  # -> используется на проде
+    uvicorn.run("main:app", reload=True)
