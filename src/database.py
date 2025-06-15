a258caceeec0741df0ab7a11a04e1dd1987c508f
Daniel <d.akipskiy@gmail.com>
2025-06-16 06:37:25 +0800

    feat(db): добавить создание сессий

--- src/database.py
+++ src/database.py
@@ -6,3 +6,7 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from src.config import settings
 
 engine = create_async_engine(settings.DB_URL)
+
+# bind-связать с движком
+# expire_on_commit - ?
+async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
