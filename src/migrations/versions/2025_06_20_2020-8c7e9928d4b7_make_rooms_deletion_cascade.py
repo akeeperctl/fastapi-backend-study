ce306c20320a92a988f851cedc12743cd33ea11c
Daniel <d.akipskiy@gmail.com>
2025-06-20 20:26:38 +0800

    fix(rooms): при удалении отеля возникала ошибка и комнаты не удалялись

---
+++ src/migrations/versions/2025_06_20_2020-8c7e9928d4b7_make_rooms_deletion_cascade.py
@@ -0,0 +1,38 @@
+"""make rooms deletion cascade
+
+Revision ID: 8c7e9928d4b7
+Revises: 70152c374304
+Create Date: 2025-06-20 20:20:27.386723
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "8c7e9928d4b7"
+down_revision: Union[str, None] = "70152c374304"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.drop_constraint(op.f("rooms_hotel_id_fkey"), "rooms", type_="foreignkey")
+    op.create_foreign_key(
+        op.f("fk_rooms_hotel_id_hotels"),
+        "rooms",
+        "hotels",
+        ["hotel_id"],
+        ["id"],
+        ondelete="CASCADE",
+    )
+
+
+def downgrade() -> None:
+    op.drop_constraint(op.f("fk_rooms_hotel_id_hotels"), "rooms", type_="foreignkey")
+    op.create_foreign_key(
+        op.f("rooms_hotel_id_fkey"), "rooms", "hotels", ["hotel_id"], ["id"]
+    )
