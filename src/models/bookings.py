23ce264223a451a76b51663ad1310791b518b9be
Daniel <d.akipskiy@gmail.com>
2025-06-20 21:36:27 +0800

    fix(booking): исправить ошибку при доб. даты с таймзоной

--- src/models/bookings.py
+++ src/models/bookings.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, DateTime
 from sqlalchemy.orm import Mapped
 from sqlalchemy.testing.schema import mapped_column
 
@@ -16,6 +16,6 @@ class BookingsOrm(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     room_id: Mapped[int] = mapped_column(ForeignKey(RoomsOrm.id))
     user_id: Mapped[int] = mapped_column(ForeignKey(UsersOrm.id))
-    date_from: Mapped[datetime]
-    date_to: Mapped[datetime]
+    date_from: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    date_to: Mapped[datetime] = mapped_column(DateTime(timezone=True))
     price: Mapped[int]
