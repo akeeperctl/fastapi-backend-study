743ae5e8db225e137c428f21dd61dd1f6b2468f0
Daniel <d.akipskiy@gmail.com>
2025-06-23 06:09:19 +0800

    feat(booking): добавить получение бронирований

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -14,7 +14,7 @@ async def create_booking(
 ):
     room = await db.rooms.get_one_or_none(id=booking_data.room_id)
     price_per_day = room.price
-    days = (booking_data.date_to - booking_data.date_from).days
+    days = max((booking_data.date_to - booking_data.date_from).days, 1)
     final_price = price_per_day * days
 
     _booking_data = BookingAddSchema(
@@ -26,3 +26,16 @@ async def create_booking(
     await db.bookings.add(_booking_data)
     await db.commit()
     return {"statis": "ok", "data": _booking_data}
+
+
+@router.get("", description="Получить все бронирования")
+async def get_bookings(db: DBDep):
+    return {"data": await db.bookings.get_all()}
+
+
+@router.get("/me", description="Получить бронирования авторизованного пользователя")
+async def get_me_bookings(
+        user_id: UserIdDep,
+        db: DBDep
+):
+    return {"data": await db.bookings.get_filtered(user_id=user_id)}
