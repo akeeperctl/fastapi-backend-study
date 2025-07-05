04d6f6a5a5552159cadf4661a8fd2700f80198ac
Daniel <d.akipskiy@gmail.com>
2025-07-05 15:36:01 +0800

    tests: добавить фикстуру пересоздания таблиц БД

--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -7,4 +7,5 @@ async def test_add_hotel():
     hotel_data = HotelAddSchema(title="Hotel 5 stars", location="Сочи")
     async with DBManager(session_factory=async_session_maker) as db:
         new_hotel_data = await db.hotels.add(hotel_data)
+        await db.commit()
         print(f"{new_hotel_data=}")
