5bc96e1e9b44a6592321787cfd0522aec5afcc27
Daniel <d.akipskiy@gmail.com>
2025-07-07 12:07:54 +0800

    tests(test_db): работа над ошибками

--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -18,6 +18,10 @@ async def test_booking_crud(db):
 
     # read
     read_booking = await db.bookings.get_one_or_none(id=added_booking.id)
+    assert read_booking
+    assert read_booking.id == added_booking.id
+    assert read_booking.room_id == room_id
+    assert read_booking.user_id == user_id
 
     # update
     update_booking_data = BookingAddRequestSchema(
@@ -26,8 +30,15 @@ async def test_booking_crud(db):
         date_to=date(year=2025, month=8, day=25),
     )
     await db.bookings.edit(data=update_booking_data, id=read_booking.id)
+    updated_booking = await db.bookings.get_one_or_none(id=read_booking.id)
+    assert updated_booking
+    assert updated_booking.id == read_booking.id
+    assert updated_booking.date_from == date(year=2025, month=8, day=20)
+    assert updated_booking.date_to == date(year=2025, month=8, day=25)
 
     # delete
     await db.bookings.delete(id=read_booking.id)
+    booking = await db.bookings.get_one_or_none(id=added_booking.id)
+    assert not booking
 
     await db.commit()
