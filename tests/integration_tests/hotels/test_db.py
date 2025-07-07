c9565530b95935318411bc0016bccf246c6d8a9d
Daniel <d.akipskiy@gmail.com>
2025-07-07 11:55:22 +0800

    tests(test_db): добавить booking_crud

--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -1,8 +1,33 @@
-from src.schemas.hotels import HotelAddSchema
+from datetime import date
+
+from src.schemas.bookings import BookingAddSchema, BookingAddRequestSchema
 
 
-async def test_add_hotel(db):
-    hotel_data = HotelAddSchema(title="Hotel 5 stars", location="Сочи")
-    new_hotel_data = await db.hotels.add(hotel_data)
+async def test_booking_crud(db):
+    user_id = (await db.users.get_all())[0].id
+    room_id = (await db.rooms.get_all())[0].id
+    booking_data = BookingAddSchema(
+        user_id=user_id,
+        room_id=room_id,
+        date_from=date(year=2025, month=8, day=10),
+        date_to=date(year=2025, month=8, day=20),
+        price=100,
+    )
+    # create
+    added_booking = await db.bookings.add(booking_data)
+
+    # read
+    read_booking = await db.bookings.get_one_or_none(id=added_booking.id)
+
+    # update
+    update_booking_data = BookingAddRequestSchema(
+        room_id=room_id,
+        date_from=date(year=2025, month=8, day=20),
+        date_to=date(year=2025, month=8, day=25),
+    )
+    await db.bookings.edit(data=update_booking_data, id=read_booking.id)
+
+    # delete
+    await db.bookings.delete(id=read_booking.id)
+
     await db.commit()
-    print(f"{new_hotel_data=}")
