5b114b9ca4d589d148730eabe231d435441cece2
Daniel <d.akipskiy@gmail.com>
2025-07-09 09:12:39 +0800

    tests(bookings): добавить параметризацию тестов

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,19 +1,31 @@
-async def test_add_booking(db, logged_in_ac):
-    room_id = (await db.rooms.get_all())[0]
-    for _ in range(5):
-        response = await logged_in_ac.post(
-            "/bookings",
-            json={
-                "room_id": room_id,
-                "date_from": "2025-07-01",
-                "date_to": "2025-07-10",
-            }
-        )
+import pytest
 
+
+@pytest.mark.parametrize("room_id, date_from, date_to, status_code", [
+    (1, "2025-07-01", "2025-07-10", 200),
+    (1, "2025-07-02", "2025-07-11", 200),
+    (1, "2025-07-03", "2025-07-12", 200),
+    (1, "2025-07-04", "2025-07-13", 200),
+    (1, "2025-07-05", "2025-07-14", 200),
+    (1, "2025-07-06", "2025-07-15", 404),
+    (1, "2025-07-07", "2025-07-16", 404),
+])
+async def test_add_booking(
+        room_id, date_from, date_to, status_code,
+        db, logged_in_ac
+):
+    response = await logged_in_ac.post(
+        "/bookings",
+        json={
+            "room_id": room_id,
+            "date_from": date_from,
+            "date_to": date_to,
+        }
+    )
+
+    assert response.status_code == status_code
+    if status_code == 200:
         result = response.json()
-
         assert isinstance(result, dict)
         assert result.get("status") == "ok"
         assert result.get("data")
-        assert response.status_code == 200
-
